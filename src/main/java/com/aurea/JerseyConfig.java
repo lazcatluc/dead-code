9aedd8a4e471fb9e9161f07ad180c6678029a9e4
Catalin Lazar <lazcatluc@gmail.com>
2017-01-20 07:44:41 +0200

    Add Swagger and swagger-ui

--- src/main/java/com/aurea/JerseyConfig.java
+++ src/main/java/com/aurea/JerseyConfig.java
@@ -9,6 +9,10 @@ import com.aurea.rest.AnalysisAlreadInProgressExceptionHandler;
 import com.aurea.rest.ProjectEndpoint;
 import com.aurea.rest.ProjectNotFoundExceptionHandler;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+
 @Component
 @ApplicationPath("api")
 public class JerseyConfig extends ResourceConfig {
@@ -17,6 +21,23 @@ public class JerseyConfig extends ResourceConfig {
         register(ProjectEndpoint.class);
         register(ProjectNotFoundExceptionHandler.class);
         register(AnalysisAlreadInProgressExceptionHandler.class);
+        configureSwagger();
     }
 
+    private void configureSwagger() {
+        // Available at localhost:port/swagger.json
+        this.register(ApiListingResource.class);
+        this.register(SwaggerSerializers.class);
+
+        BeanConfig config = new BeanConfig();
+        config.setConfigId("swagger-dead-code");
+        config.setTitle("Swagger for Dead Code");
+        config.setVersion("v1");
+        config.setContact("Catalin Lazar <catalin.lazar@aurea.com>");
+        config.setSchemes(new String[] { "http", "https" });
+        config.setBasePath("api");
+        config.setResourcePackage(ProjectEndpoint.class.getPackage().getName());
+        config.setPrettyPrint(true);
+        config.setScan(true);
+    }
 }
--- src/main/java/com/aurea/rest/ProjectEndpoint.java
+++ src/main/java/com/aurea/rest/ProjectEndpoint.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.aurea.model.Project;
 import com.aurea.model.ProjectStatus;
@@ -22,8 +22,12 @@ import com.aurea.repo.InitializedProjects;
 import com.aurea.repo.ProjectCleaner;
 import com.aurea.und.ProjectAnalyzer;
 
-@Component
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
 @Path("/projects")
+@Api(value = "Projects resource", produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
 public class ProjectEndpoint {
     @Autowired
     private InitializedProjects projectRepository;
@@ -34,20 +38,23 @@ public class ProjectEndpoint {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Gets the list of projects", response = RestProject.class)
     public List<RestProject> getAllProjects() {
         return projectRepository.findAllInitialized().stream().map(RestProject::new).collect(Collectors.toList());
     }
-    
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Adds a project based on a git repository url", response = RestProject.class)
     public RestProject addProject(ProjectUrl projectUrl) {
         return new RestProject(projectAnalyzer.addProject(projectUrl.getUrl()));
     }
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{projectId}")
+    @ApiOperation(value = "Shows the most recent status and defects of the project with the specific id", response = RestProject.class)
     public RestProject getAllProjects(@PathParam("projectId") String projectId) {
         Project project = projectRepository.findOneInitialized(projectId);
         if (project == null) {
@@ -55,10 +62,11 @@ public class ProjectEndpoint {
         }
         return new RestProject(project);
     }
-    
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{projectId}")
+    @ApiOperation(value = "Re-run the analysis for the project with the specific id", response = RestProject.class)
     public RestProject updateProject(@PathParam("projectId") String projectId) {
         Project project = projectRepository.findOneInitialized(projectId);
         if (project == null) {
@@ -70,9 +78,10 @@ public class ProjectEndpoint {
         Project updatedProject = projectAnalyzer.updateProject(project);
         return new RestProject(updatedProject);
     }
-    
+
     @DELETE
     @Path("{projectId}")
+    @ApiOperation(value = "Delete and clean-up the project with the specific id", response = RestProject.class)
     public Response deleteProject(@PathParam("projectId") String projectId) {
         projectCleaner.clean(projectId);
         return Response.ok().build();
